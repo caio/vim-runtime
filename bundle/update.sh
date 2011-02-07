efe8330ae482a8cc3b002e37d5ffab0a21a7f09d
Caio Romão <caioromao@gmail.com>
2011-02-07 15:21:10 -0200

    Avoid subshell creation on while loop

--- bundle/update.sh
+++ bundle/update.sh
@@ -20,12 +20,12 @@ log "Updating each bundle"
 run git submodule foreach git pull
 
 updates=0
-git status --porcelain | grep bundle| grep M|cut -d/ -f 2|
-grep -v update.sh| while read BUNDLE; do
+while read BUNDLE; do
     updates=$((updates + 1))
     log "Adding updated ${BUNDLE} version"
     run git add bundle/${BUNDLE}
     git commit -m "Update ${BUNDLE} plugin"
-done
+done < <(git status --porcelain| grep bundle| grep M| cut -d/ -f 2|
+grep -v update.sh)
 
 [ "${updates}" == "0" ] && log "Nothing done" || log "${updates} updated plugins"
